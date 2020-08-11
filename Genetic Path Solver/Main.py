7a8ec9c295b02b3e601720c6e8daec992343eea2
Jose Pablo Munoz <jpmunoz@estudiantec.cr>
2020-08-10 22:11:21 -0600

    Added last generation cycle to GUI

--- Genetic Path Solver/Main.py
+++ Genetic Path Solver/Main.py
@@ -3,7 +3,7 @@ from GUI import *
 import sys
 
 def pathSolver():
-    global INITIAL_POPULATION, MUTATION_PROBABILITY_HARDWARE, MUTATION_PROBABILITY_BEHAVIOUR, GEN_COUNT, GEN_ARCHIVE, GEN_NUMBERS, ADAPTABILITY_ARCHIVE, RUNNING, GRAPH
+    global INITIAL_POPULATION, MUTATION_PROBABILITY_HARDWARE, MUTATION_PROBABILITY_BEHAVIOUR, GEN_COUNT, GEN_ARCHIVE, GEN_NUMBERS, ADAPTABILITY_ARCHIVE, RUNNING, GRAPH, SELECTED_GEN_INDEX, SELECTED_ROBOT
     currentGeneration = initGeneration(INITIAL_POPULATION)
     while RUNNING:
         for robot in currentGeneration:
@@ -18,12 +18,20 @@ def pathSolver():
         GRAPH = plot(GEN_NUMBERS, ADAPTABILITY_ARCHIVE, "b")
         if RUNNING:
             currentGeneration = crossGeneration(currentGeneration, totalAdaptability, MUTATION_PROBABILITY_HARDWARE, MUTATION_PROBABILITY_BEHAVIOUR)
-    reset()
-
-def reset():
-    print("settings reset")
+    SELECTED_GEN_INDEX = GEN_COUNT - 1
+    SELECTED_ROBOT = GEN_ARCHIVE[SELECTED_GEN_INDEX][SELECTED_ROB_INDEX]
 
 def start():
+    global GEN_COUNT, GEN_ARCHIVE, GEN_NUMBERS, ADAPTABILITY_ARCHIVE, GRAPH, RUNNING, SELECTED_ROB_INDEX, SELECTED_GEN_INDEX, SELECTED_ROBOT
+    GEN_COUNT = 0
+    GEN_ARCHIVE = []
+    GEN_NUMBERS = []
+    ADAPTABILITY_ARCHIVE = []
+    GRAPH = None
+    SELECTED_ROB_INDEX = 0
+    SELECTED_GEN_INDEX = 0
+    SELECTED_ROBOT = None
+    RUNNING = True
     solverThread.start()
 
 # Globals
@@ -35,6 +43,9 @@ GEN_ARCHIVE = []
 GEN_NUMBERS = []
 ADAPTABILITY_ARCHIVE = []
 GRAPH = None
+SELECTED_ROB_INDEX = 0
+SELECTED_GEN_INDEX = 0
+SELECTED_ROBOT = None
 RUNNING = True
 
 # thread setup
@@ -43,6 +54,7 @@ solverThread = threading.Thread(target = pathSolver, daemon = True)
 # buttons
 startButton = Button(COLOR_BROWN, 250, 30, 80, 40, 20, "START")
 stopButton = Button(COLOR_BROWN, 250, 90, 80, 40, 20, "STOP")
+cycleButton = Button(COLOR_BROWN, 850, 30, 180, 40, 20, "CYCLE GENERATION")
 
 while True:
 
@@ -57,26 +69,35 @@ while True:
                 start()
             elif stopButton.isOver(pos):
                 RUNNING = False
+            elif cycleButton.isOver(pos):
+                if not RUNNING and SELECTED_ROBOT is not None:
+                    if SELECTED_ROB_INDEX + 1 >= len(GEN_ARCHIVE[SELECTED_GEN_INDEX]):
+                        SELECTED_ROB_INDEX = 0
+                    else:
+                        SELECTED_ROB_INDEX += 1
+                    SELECTED_ROBOT = GEN_ARCHIVE[SELECTED_GEN_INDEX][SELECTED_ROB_INDEX]
 
     WINDOW.fill(COLOR_CREAM)
 
     drawMatrix(MAP, 30, 30, 10)
-
     startButton.draw(COLOR_BLACK)
     stopButton.draw(COLOR_BLACK)
-
-    drawText("BEST RESULT:", 20, 250, 150)
 
     if GRAPH is not None:
         WINDOW.blit(GRAPH, (630, 250))
 
     bestRobot = getBestRobot()
     if bestRobot is not None:
+        drawText("BEST RESULT:", 20, 250, 150)
         drawMatrix(bestRobot.progressMap, 30, 250, 25)
         infoHardware = bestRobot.getInfoHardware()
         infoAdaptability = bestRobot.getInfoAdaptability()
         drawText(infoHardware, 20, 250, 180)
         drawText(infoAdaptability, 20, 250, 210)
+
+    if not RUNNING and SELECTED_ROBOT is not None:
+        cycleButton.draw(COLOR_BLACK)
+        drawMatrix(SELECTED_ROBOT.progressMap, 1050, 30, 10)
 
     py.display.flip()
     CLOCK.tick(60)
